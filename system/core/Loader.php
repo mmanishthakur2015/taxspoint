92704d1a806c967e7d68d6c102b5fc836297bcd6
mmanishthakur2015 <mmanishthakur2015@gmail.com>
2026-09-12 20:11:02 +0530

    fix: remove recursive __set from CI_Loader to prevent recursion 500 error

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -108,13 +108,4 @@ class CI_Loader {
 	public function helper($helpers = array()) {
 		return $this;
 	}
-
-	public function __get($key) {
-		$CI =& get_instance();
-		return isset($CI->$key) ? $CI->$key : NULL;
-	}
-
-	public function __set($key, $val) {
-		$this->$key = $val;
-	}
 }
